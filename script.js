bc14c7c3736b7b2d3e47cce4ef4f451d616cec2c
Juan Gomez <gomex086@gmail.com>
2023-10-17 12:18:37 -0500

    Add description

--- script.js
+++ script.js
@@ -1,13 +1,24 @@
 const yugiohCardList = document.getElementById('yugioh-card-list');
-const apiUrl = "https://db.ygoprodeck.com/api/v7/cardinfo.php"; // Remove the "?num=24&offset=0" to get all cards
+const apiUrl = "https://db.ygoprodeck.com/api/v7/cardinfo.php";
+let yugiohCards = [];
 
-const searchInput = document.getElementById('search'); // Get the search input element
-const pagination = document.getElementById('pagination');
+const searchInput = document.getElementById('search');
+const searchButton = document.getElementById('searchButton');
 
-const itemsPerPage = 300; // Number of items per page (changed to 5)
-let yugiohCards = []; // To store the fetched Yu-Gi-Oh cards
+searchButton.addEventListener('click', performSearch);
 
-// Function to create a Yu-Gi-Oh card
+searchInput.addEventListener('keypress', (event) => {
+    if (event.key === 'Enter') {
+        performSearch();
+    }
+});
+
+function performSearch() {
+    const searchTerm = searchInput.value.toLowerCase();
+    const filteredCards = yugiohCards.filter((card) => card.name.toLowerCase().includes(searchTerm));
+    renderYuGiOhCards(filteredCards);
+}
+
 function createYuGiOhCard(card) {
     const cardElement = document.createElement('div');
     cardElement.classList.add('card');
@@ -34,23 +45,16 @@ function createYuGiOhCard(card) {
     return cardElement;
 }
 
-// Function to fetch Yu-Gi-Oh card data from the API
 function fetchYuGiOhCardData() {
     fetch(apiUrl)
         .then((response) => response.json())
         .then((data) => {
             yugiohCards = data.data;
-            renderPagination(yugiohCards);
-            showPage(1, yugiohCards);
-
-            searchInput.addEventListener('input', () => {
-                filterYuGiOhCards(yugiohCards);
-            });
+            renderYuGiOhCards(yugiohCards);
         })
         .catch((error) => console.error(error));
 }
 
-// Function to display Yu-Gi-Oh card details in the modal
 function displayYuGiOhCardDetails(card) {
     const modalTitle = document.getElementById('yugiohCardModalLabel');
     const modalBody = document.getElementById('yugiohCardDetails');
@@ -63,43 +67,18 @@ function displayYuGiOhCardDetails(card) {
         <p>Level/Rank: ${card.level}</p>
         <p>Attack: ${card.atk}</p>
         <p>Defense: ${card.def}</p>
+        <p class="card-description">${card.desc}</p>
     `;
 
     $('#yugiohCardModal').modal('show');
 }
 
-// Function to filter Yu-Gi-Oh cards based on search input
-function filterYuGiOhCards(cards) {
-    const searchTerm = searchInput.value.toLowerCase();
-    const filteredCards = cards.filter((card) => card.name.toLowerCase().includes(searchTerm));
-    renderPagination(filteredCards);
-    showPage(1, filteredCards);
-}
-
-// Function to render pagination
-function renderPagination(cards) {
-    pagination.innerHTML = '';
-    const numPages = Math.ceil(cards.length / itemsPerPage);
-    for (let i = 1; i <= numPages; i++) {
-        const li = document.createElement('li');
-        li.className = 'page-item';
-        li.innerHTML = `<a class="page-link" href="#">${i}</a>`;
-        li.addEventListener('click', () => showPage(i, cards));
-        pagination.appendChild(li);
-    }
-}
-
-// Function to show a specific page of cards
-function showPage(pageNumber, cards) {
+function renderYuGiOhCards(cards) {
     yugiohCardList.innerHTML = '';
-    const startIndex = (pageNumber - 1) * itemsPerPage;
-    const endIndex = startIndex + itemsPerPage;
-    const pageCards = cards.slice(startIndex, endIndex);
-    pageCards.forEach((card) => {
+    cards.forEach((card) => {
         const cardElement = createYuGiOhCard(card);
         yugiohCardList.appendChild(cardElement);
     });
 }
 
-// Populate the Yu-Gi-Oh card list
 fetchYuGiOhCardData();
