6e7eee776689a838a6de488ca67956a1084f1edd
Juan Gomez <gomex086@gmail.com>
2023-10-20 13:01:49 -0500

    fixed search bar

--- script.js
+++ script.js
@@ -1,112 +1,114 @@
-const yugiohCardList = document.getElementById('yugioh-card-list');
-const apiUrl = "https://db.ygoprodeck.com/api/v7/cardinfo.php";
-let yugiohCards = [];
-let currentPage = 1;
-let numPages = 0;
+document.addEventListener("DOMContentLoaded", function() {
+    const yugiohCardList = document.getElementById('yugioh-card-list');
+    const apiUrl = "https://db.ygoprodeck.com/api/v7/cardinfo.php";
+    let yugiohCards = [];
+    let currentPage = 1;
+    let numPages = 0;
 
-const searchInput = document.getElementById('search');
-const searchButton = document.getElementById('searchButton');
+    const searchInput = document.getElementById('search');
+    const searchButton = document.getElementById('searchButton');
 
-searchButton.addEventListener('click', performSearch);
+    searchButton.addEventListener('click', performSearch);
 
-searchInput.addEventListener('keypress', (event) => {
-    if (event.key === 'Enter') {
-        performSearch();
+    searchInput.addEventListener('keypress', (event) => {
+        if (event.key === 'Enter') {
+            performSearch();
+        }
+    });
+
+    document.getElementById('prevPage').addEventListener('click', previousPage);
+    document.getElementById('nextPage').addEventListener('click', nextPage);
+
+    function performSearch() {
+        const searchTerm = searchInput.value.toLowerCase();
+        const filteredCards = yugiohCards.filter((card) => card.name.toLowerCase().includes(searchTerm));
+        showYuGiOhCards(filteredCards);
     }
+
+    function createYuGiOhCard(card) {
+        const cardElement = document.createElement('div');
+        cardElement.className = 'card';
+
+        const image = document.createElement('img');
+        image.src = card.card_images[0].image_url;
+        image.alt = card.name;
+        image.className = 'card-image';
+
+        const title = document.createElement('h5');
+        title.className = 'card-title';
+        title.textContent = card.name;
+
+        const cardBody = document.createElement('div');
+        cardBody.className = 'card-body';
+
+        cardBody.appendChild(image);
+        cardBody.appendChild(title);
+        cardElement.appendChild(cardBody);
+
+        cardElement.addEventListener('click', () => {
+            showYuGiOhCardDetails(card);
+        });
+
+        return cardElement;
+    }
+
+    function fetchYuGiOhCardData() {
+        fetch(apiUrl)
+            .then((response) => response.json())
+            .then((data) => {
+                yugiohCards = data.data;
+                numPages = Math.ceil(yugiohCards.length / 51);
+                showYuGiOhCards(yugiohCards);
+            })
+            .catch((error) => console.error(error));
+    }
+
+    function showYuGiOhCardDetails(card) {
+        const modalTitle = document.getElementById('yugiohCardModalLabel');
+        const modalBody = document.getElementById('yugiohCardDetails');
+
+        modalTitle.textContent = `Yu-Gi-Oh Card Details - ${card.name}`;
+
+        modalBody.innerHTML = `
+            <img src="${card.card_images[0].image_url}" alt="${card.name}" class="card-image">
+            <p>Type: ${card.type}</p>
+            <p>Attribute: ${card.attribute}</p>
+            <p>Level/Rank: ${card.level}</p>
+            <p>Attack: ${card.atk}</p>
+            <p>Defense: ${card.def}</p>
+            <p class="card-description">${card.desc}</p>
+        `;
+
+        $('#yugiohCardModal').modal('show');
+    }
+
+    function showYuGiOhCards(cards) {
+        yugiohCardList.innerHTML = '';
+        const start = (currentPage - 1) * 51;
+        const end = start + 51;
+        const paginatedCards = cards.slice(start, end);
+
+        paginatedCards.forEach((card) => {
+            const cardElement = createYuGiOhCard(card);
+            yugiohCardList.appendChild(cardElement);
+        });
+    }
+
+    function previousPage() {
+        currentPage--;
+        if (currentPage < 1) {
+            currentPage = numPages;
+        }
+        showYuGiOhCards(yugiohCards);
+    }
+
+    function nextPage() {
+        currentPage++;
+        if (currentPage > numPages) {
+            currentPage = 1;
+        }
+        showYuGiOhCards(yugiohCards);
+    }
+
+    fetchYuGiOhCardData();
 });
-
-document.getElementById('prevPage').addEventListener('click', previousPage);
-document.getElementById('nextPage').addEventListener('click', nextPage);
-
-function performSearch() {
-    const searchTerm = searchInput.value.toLowerCase();
-    const filteredCards = yugiohCards.filter((card) => card.name.toLowerCase().includes(searchTerm));
-    showYuGiOhCards(filteredCards);
-}
-
-function createYuGiOhCard(card) {
-    const cardElement = document.createElement('div');
-    cardElement.className = 'card';
-
-    const image = document.createElement('img');
-    image.src = card.card_images[0].image_url;
-    image.alt = card.name;
-    image.className = 'card-image';
-
-    const title = document.createElement('h5');
-    title.className = 'card-title';
-    title.textContent = card.name;
-
-    const cardBody = document.createElement('div');
-    cardBody.className = 'card-body';
-
-    cardBody.appendChild(image);
-    cardBody.appendChild(title);
-    cardElement.appendChild(cardBody);
-
-    cardElement.addEventListener('click', () => {
-        showYuGiOhCardDetails(card);
-    });
-
-    return cardElement;
-}
-
-function fetchYuGiOhCardData() {
-    fetch(apiUrl)
-        .then((response) => response.json())
-        .then((data) => {
-            yugiohCards = data.data;
-            numPages = Math.ceil(yugiohCards.length / 51);
-            showYuGiOhCards(yugiohCards);
-        })
-        .catch((error) => console.error(error));
-}
-
-function showYuGiOhCardDetails(card) {
-    const modalTitle = document.getElementById('yugiohCardModalLabel');
-    const modalBody = document.getElementById('yugiohCardDetails');
-
-    modalTitle.textContent = `Yu-Gi-Oh Card Details - ${card.name}`;
-
-    modalBody.innerHTML = `
-        <img src="${card.card_images[0].image_url}" alt="${card.name}" class="card-image">
-        <p>Type: ${card.type}</p>
-        <p>Attribute: ${card.attribute}</p>
-        <p>Level/Rank: ${card.level}</p>
-        <p>Attack: ${card.atk}</p>
-        <p>Defense: ${card.def}</p>
-        <p class="card-description">${card.desc}</p>
-    `;
-
-    $('#yugiohCardModal').modal('show');
-}
-
-function showYuGiOhCards(cards) {
-    yugiohCardList.innerHTML = '';
-    const start = (currentPage - 1) * 51;
-    const end = start + 51;
-    const paginatedCards = cards.slice(start, end);
-
-    paginatedCards.forEach((card) => {
-        const cardElement = createYuGiOhCard(card);
-        yugiohCardList.appendChild(cardElement);
-    });
-}
-
-function previousPage() {
-    currentPage--;
-    if (currentPage < 1) {
-        currentPage = numPages;
-    }
-    showYuGiOhCards(yugiohCards);
-}
-
-function nextPage() {
-    currentPage++;
-    if (currentPage > numPages) {
-        currentPage = 1;
-    }
-    showYuGiOhCards(yugiohCards);
-}
-
-fetchYuGiOhCardData();
